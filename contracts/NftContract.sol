8e7c5f8f874751d196e1aeb0977f7565a2903b10
seniraliev <seniraliev94@gmail.com>
2023-04-08 08:35:43 -0600

    fix contract event

--- contracts/NftContract.sol
+++ contracts/NftContract.sol
@@ -6,11 +6,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract NftContract is ERC721URIStorage {
   uint256 private s_tokenCounter;
 
-  event NftContractCreated(address indexed creator, address indexed contractAddress);
+  event NftContractCreated(
+    address indexed creator,
+    address indexed contractAddress,
+    string name,
+    string symbol
+  );
 
   constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
     s_tokenCounter = 0;
-    emit NftContractCreated(msg.sender, address(this));
+    emit NftContractCreated(msg.sender, address(this), name_, symbol_);
   }
 
   function mintNft(string memory tokenURI) public returns (uint256) {
